dea7823f3cb4a76935179c9d9a95f33d232f2ece
Feranmi Akinlade <uxferanmi@gmail.com>
2020-08-20 18:42:08 +0100

    Add authorization headers

--- lighthouserc.ci.js
+++ lighthouserc.ci.js
@@ -5,12 +5,15 @@ module.exports = {
 			startServerReadyPattern: "Available",
             numberOfRuns: 3,
 			startServerReadyTimeout: 10000,
+			settings: {
+				extraHeaders: JSON.stringify({authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTgwMTg4MTIsInN1YiI6ImM1YWNjMDAxLTBhYWMtNDJmYS1iMjIwLWE3OGJlMTlhODM5MyJ9.l1yPPetijSpesKoJ_pEMDd__HWIpT3l9DIKhuaET0R4'}),
+			},
 		},
 		assert: {
 		  assertions: {
 			'categories:performance': ['error', {minScore: 1}],
-			'categories:accessibility': ['warn', {minScore: 1}]
-		  }
+			'categories:accessibility': ['warn', {minScore: 1}],
+		  },
 		},
 		upload: {
 			target: 'temporary-public-storage',
--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,10 @@ module.exports = {
 			staticDistDir: './www',
 			url: ['http://localhost', 'http://localhost/tabs/tab2'],
 			numberOfRuns: 3,
-			isSinglePageApplication: true
+			isSinglePageApplication: true,
+			settings: {
+				extraHeaders: JSON.stringify({authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTgwMTg4MTIsInN1YiI6ImM1YWNjMDAxLTBhYWMtNDJmYS1iMjIwLWE3OGJlMTlhODM5MyJ9.l1yPPetijSpesKoJ_pEMDd__HWIpT3l9DIKhuaET0R4'}),
+			},
 		},
 		assert: {
 		  assertions: {
